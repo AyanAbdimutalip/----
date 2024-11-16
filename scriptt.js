eba5a3b22f4890b1388a03950c83903a693aef5e
Ayan Abdimutalip <aianabdimutalip6@gmail.com>
2024-11-16 21:20:43 +0500

    first commit

---
+++ scriptt.js
@@ -0,0 +1,54 @@
+let truePassword = "amjilt_the_best";
+let shans = 5;
+
+// for (let g = 5; g > 0; g--) {
+//     let password = prompt("Enter your password:  ");
+    
+//     if (password == truePassword) {
+//         alert("True");
+//         break;
+//     } else if (g == 1) {
+//         alert("Too many attempts");
+//     } else {
+//         alert("Қате пароль. Қайтадан көріңіз.");
+//     }
+// }
+
+
+
+// while (shans > 0) {
+//     let password = prompt("Enter your password:  " + shans);
+    
+//     if (password == truePassword) {
+//         alert("Құттықтаймыз! Сіз дұрыс пароль енгіздіңіз.");
+//         break;
+//     } else {
+//         shans--;
+//         if (shans == 0) {
+//             alert("Too many attempts");
+//         } else {
+//             alert("Қате пароль. Қайтадан көріңіз.");
+//         }
+//     }
+// }
+
+
+// do {
+//     let password = prompt("Enter your password:  " + shans);
+    
+//     if (password == truePassword) {
+//         alert("Құттықтаймыз! Сіз дұрыс пароль енгіздіңіз.");
+//         break;
+//     } else {
+//         shans--;
+//         if (shans == 0) {
+//             alert("Too many attempts");
+//         } else {
+//             alert("Қате пароль. Қайтадан көріңіз.");
+//         }
+//     }
+// } while (shans > 0);
+
+
+
+
